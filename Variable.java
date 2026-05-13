98b3da9030b06a5ff99127ed6e1626a0942ff1fa
shubham karad <shubham.karad12@gmail.com>
2026-05-13 22:51:51 +0530

    Variable Full Info

---
+++ Variable.java
@@ -0,0 +1,37 @@
+public class Variable {
+
+    public static void main(String args[]) {
+
+        // byte → 1 byte
+        byte b = 10;
+        System.out.println("Byte: " + b);
+
+        // short → 2 bytes
+        short s = 200;
+        System.out.println("Short: " + s);
+
+        // int → 4 bytes
+        int num = 1000;
+        System.out.println("Integer: " + num);
+
+        // long → 8 bytes
+        long l = 12345L;
+        System.out.println("Long: " + l);
+
+        // float → 4 bytes
+        float f = 3.14f;
+        System.out.println("Float: " + f);
+
+        // double → 8 bytes
+        double d = 56.78;
+        System.out.println("Double: " + d);
+
+        // char → 2 bytes
+        char c = 'A';
+        System.out.println("Character: " + c);
+
+        // boolean → true or false
+        boolean flag = true;
+        System.out.println("Boolean: " + flag);
+    }
+}
