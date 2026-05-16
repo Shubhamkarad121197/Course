4ece1310e799442a86db73266cadfad173892092
shubham karad <shubham.karad12@gmail.com>
2026-05-16 15:10:25 +0530

    Class , object constructor

---
+++ CarInfo.java
@@ -0,0 +1,23 @@
+class Car{
+    String brand;
+    int speed;
+
+    Car(String brand,int speed){
+        this.brand=brand;
+        this.speed=speed;
+    }
+    void display(){
+        System.out.println("Brand:"+brand);
+        System.out.println("speed:"+speed);
+    }
+}
+
+public class CarInfo{
+    public static void main(String args[]){
+        Car c1=new Car("Audi",5000);
+        Car c2=new Car("Mercedez",4000);
+
+        c1.display();
+        c2.display();
+    }
+}
