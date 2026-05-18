626de9b5d4d41dd2872ec49069814d30947bda08
shubham karad <shubham.karad12@gmail.com>
2026-05-18 23:11:21 +0530

    String Traversal

---
+++ StringTraversal.java
@@ -0,0 +1,8 @@
+class StringTraversal{
+    public static void main(String args[]){
+        String str="Shubham";
+        for(int i=0;i<str.length();i++){
+            System.out.println(str.charAt(i));
+        }
+    }
+}
