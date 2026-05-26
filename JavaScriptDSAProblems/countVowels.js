e3f5cc209378a42db64153183cc68f0d10b36da6
shubham karad <shubham.karad12@gmail.com>
2026-05-27 00:45:02 +0530

    count vowels

---
+++ JavaScriptDSAProblems/countVowels.js
@@ -0,0 +1,22 @@
+// Input:
+// "shubham"
+
+// Output:
+// 2
+
+let str="shubham";
+function findVowelsCount(str){
+    let vowels=new Set(['A','E','I','O','U',"a",'e','i','o','u']);
+    let count=0;
+    let consonants=0
+    for(let char of str){
+        if(vowels.has(char)){
+            count++;
+        }else{
+            consonants++;
+        }
+    }
+    return `vowels:${count}:consonants,${consonants}`;
+}
+
+console.log(findVowelsCount(str));
