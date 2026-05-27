3c918abcfebe3a6255ae09d9f0473ae3aec4a860
shubham karad <shubham.karad12@gmail.com>
2026-05-28 00:23:39 +0530

    MovesAllZeroes

--- JavaScriptDSAProblems/movesAllZeros.js
+++ JavaScriptDSAProblems/movesAllZeros.js
@@ -9,7 +9,7 @@ function movesZeros(arr){
     
     let count=0;
     for(let i=0;i<arr.length;i++){
-       if(arr[i]>0){
+       if(arr[i]!==0){
            newArr[count]=arr[i]
             count++;
        }
