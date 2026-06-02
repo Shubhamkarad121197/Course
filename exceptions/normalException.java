2a1c32267e57f17b39a54e7006d2c8fe6bce5258
shubham karad <shubham.karad12@gmail.com>
2026-06-02 23:57:04 +0530

    indexBound

--- exceptions/normalException.java
+++ exceptions/normalException.java
@@ -2,12 +2,18 @@
 public class normalException {
     public static void main(String[] args) {
         int i=0;
+        int[] arr=new int[5];
         try {
-            int ans=18/i;
-            System.out.println(ans);
+            // int ans=18/i;
+            // System.out.println(ans);
+            System.err.println(arr[2]);
+            System.err.println(arr[6]);
         }
         catch (ArithmeticException e){
               System.out.println("Dont devide by zero");
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+              System.out.println("Stay in your limit");
         }
          catch (Exception e) {
             System.out.println("Something went wrongs");
