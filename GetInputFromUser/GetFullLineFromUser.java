80cb3d5c6d53e51d9285bd3e0c483481047ecd00
shubham karad <shubham.karad12@gmail.com>
2026-06-06 00:55:23 +0530

    GetFullIneFromUser

---
+++ GetInputFromUser/GetFullLineFromUser.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class GetFullLineFromUser {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("What is Your name:");
+        String S1=sc.nextLine();
+
+        System.out.println(S1);
+
+    }
+}
