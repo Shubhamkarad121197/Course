8995e88117178040828fa34453a76de33aa2fff6
shubham karad <shubham.karad12@gmail.com>
2026-06-09 23:18:33 +0530

    multipleTHread

---
+++ Thread/MultipleThread.java
@@ -0,0 +1,27 @@
+class A extends Thread{
+    public void run(){
+        for(int i = 1; i <= 5; i++) {
+            System.out.println("A : " + i);
+        }
+    }
+}
+
+class B extends Thread{
+    public void run(){
+       for(int i = 1; i <= 5; i++) {
+            System.out.println("B : " + i);
+        }
+    }
+}
+
+
+
+public class MultipleThread {
+    public static void main(String args[]){
+        A a = new A();
+        B b = new B();
+
+        a.start();
+        b.start();
+    }
+}
