2b61f1dc0ee30b36f021938315e496663619da92
shubham karad <shubham.karad12@gmail.com>
2026-06-11 00:14:24 +0530

    List interface

---
+++ Collections/ListInterFace.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class  ListInterFace{
+    public static void main(String[] args) {
+        List<String> list=new ArrayList();
+        list.add("Shubham");
+        list.add("karad");
+        list.add("Kartikee");
+        list.add("Pawar");
+
+        System.out.println(list);
+    }
+}
