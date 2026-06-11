d69097d070a660737660f52ffd87d30e20caf6b2
shubham karad <shubham.karad12@gmail.com>
2026-06-11 23:39:18 +0530

    LinkList

---
+++ Collections/LinkListInterface.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class LinkListInterface {
+    public static void main(String[] args) {
+        LinkedList<String> list=new LinkedList<>();
+        list.add("Suraj");
+        list.add("Karan");
+        list.add("Vishal");
+
+        System.out.println(list);
+    }
+}
