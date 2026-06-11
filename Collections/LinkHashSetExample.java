98d989c15614cbdb260fc2957cfb3f07050e2f4b
shubham karad <shubham.karad12@gmail.com>
2026-06-11 23:45:19 +0530

    LinkedHashSet

---
+++ Collections/LinkHashSetExample.java
@@ -0,0 +1,16 @@
+
+import java.util.LinkedHashSet;
+
+public class LinkHashSetExample {
+    public static void main(String[] args) {
+        LinkedHashSet<Integer> nums=new LinkedHashSet<>();
+
+        nums.add(30);
+        nums.add(10);
+        nums.add(5);
+
+        System.out.println(nums);
+        
+        
+    }
+}
