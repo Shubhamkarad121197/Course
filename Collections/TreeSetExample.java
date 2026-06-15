1d4552713cdd55b8560413db7e6a2fde991d75e2
shubham karad <shubham.karad12@gmail.com>
2026-06-15 22:54:38 +0530

    TreeSet

---
+++ Collections/TreeSetExample.java
@@ -0,0 +1,14 @@
+
+import java.util.TreeSet;
+
+public class TreeSetExample {
+    public static void main(String args[]){
+        TreeSet<Integer> nums=new TreeSet<>();
+        nums.add(34);
+        nums.add(10);
+        nums.add(25);
+        nums.add(90);
+
+        System.out.println(nums);
+    }
+}
