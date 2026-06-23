b4a49bcf78389f3946a91a68510b2ab5a9f09c81
shubham karad <shubham.karad12@gmail.com>
2026-06-23 23:22:55 +0530

    plusOne

---
+++ JavaScriptDSAProblems/plusOne.js
@@ -0,0 +1,20 @@
+var plusOne = function(digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+
+        digits[i] = 0;
+    }
+
+    digits.unshift(1);
+    return digits;
+};
+
+
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
